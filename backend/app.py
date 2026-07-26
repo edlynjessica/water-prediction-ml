3c7bdb2cca96f738e0e3e47a873cc11989bf5fd9
edlynjessica <edlynjessica09@gmail.com>
2026-07-26 16:49:43 +0530

    Implememted FastAPI Backend

--- backend/app.py
+++ backend/app.py
@@ -3,14 +3,25 @@ from fastapi import FastAPI
 from pydantic import BaseModel # basemodel -> form validation
 #It tells FastAPI: "Whenever somebody calls /predict, these fields must exist."
 
-import pickle
+from fastapi.middleware.cors import CORSMiddleware # because we are deploying backend in Render and frontend separately in GitHub Pages, we need to allow cross-origin requests.
+
+import joblib
 import numpy as np
 
 app = FastAPI() # creating my backend app
 
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],   # We'll tighten this later
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 # Load the trained model once when the server starts
-with open("model.pkl", "rb") as f:
-    model = pickle.load(f)
+model = joblib.load("model.pkl")
 
 
 @app.get("/") 
@@ -32,6 +43,25 @@ class WaterInput(BaseModel):
 
 @app.post("/predict")
 def predict(data: WaterInput):
+
+    input_data = np.array([[
+        data.tank_capacity,
+        data.current_level,
+        data.residents,
+        data.daily_usage,
+        data.rainfall
+    ]])
+
+    prediction = model.predict(input_data)[0]
+
+    if prediction > 5:
+        alert = "SAFE"
+    elif prediction > 2:
+        alert = "WARNING"
+    else:
+        alert = "CRITICAL"
+
     return {
-        "received": data
+        "days_left": round(float(prediction), 2),
+        "alert": alert
     }
