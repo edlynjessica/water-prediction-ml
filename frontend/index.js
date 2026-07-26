4872fbc275aff4236f822ca14d83384e4c15ae80
edlynjessica <edlynjessica09@gmail.com>
2026-07-26 17:56:31 +0530

    Updated deployed API endpoint and homepage

--- frontend/index.js
+++ frontend/index.js
@@ -16,7 +16,7 @@ async function predict() {
 
     try {
 
-        const response = await fetch("http://127.0.0.1:8000/predict", {
+        const response = await fetch("https://water-prediction-ml.onrender.com/predict", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
